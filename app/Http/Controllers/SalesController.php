bc33d8d64ab1f6e2f3dfe8659b509da848c17f2f
Jason Nugraha <jason.nugraha97@gmail.com>
2021-05-04 22:29:49 +0700

    initial commit

---
+++ app/Http/Controllers/SalesController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sales;
+use Illuminate\Http\Request;
+
+class SalesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sales = Sales::latest()->paginate(5);
+
+        return view('sales.index', compact('sales'))->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('sales.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'ProductName' => 'required',
+            'SalesQuantity' => 'required',
+            'ProductPrice' => 'required',
+            'TotalPrice' => 'required'
+        ]);
+
+        Sales::create($request->all());
+
+        return redirect()->route('sales.index')->with('success', 'Sales record added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sales  $sales
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sales $sales)
+    {
+        return view ('sales.show', compact('sales'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sales  $sales
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sales $sales)
+    {
+        return view('sales.edit', compact('sales'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Sales  $sales
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sales $sales)
+    {
+        $request->validate([
+            'ProductName' => 'required',
+            'SalesQuantity' => 'required',
+            'ProductPrice' => 'required',
+            'TotalPrice' => 'required'
+        ]);
+
+        $sales->update($request->all());
+
+        return redirect()->route('sales.index')->with('success', 'Sales record updaed');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sales  $sales
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sales $sales)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_05_04_140940_create_sales_records.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesRecords extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_records', function (Blueprint $table) {
+            $table->id('SalesId');
+            $table->string('ProductName');
+            $table->integer('SalesQuantity');
+            $table->integer('ProductPrice');
+            $table->integer('TotalPrice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_records');
+    }
+}
